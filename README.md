2a6e10d82e580a012fa3440a4ac65447458e3adf
BelenBD <danielaballon@hotmail.com>
2024-01-18 15:50:30 -0300

    actualización de estructura con librerias

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Porfolio
+unDraw.com
---
+++ app.js
@@ -0,0 +1,17 @@
+const typed= new Typed('.typed', {
+
+stringsElement:'#cadenas-texto',
+typeSpeed: 75,
+startDelay: 300,
+backSpeed: 75,
+smartBackspace: false,
+shuffle: false,
+backDelay: 1500,
+loop: true,
+loopCount: false,
+showCursor: true,
+cursorChar: '|',
+contentType: 'html',
+
+
+});
